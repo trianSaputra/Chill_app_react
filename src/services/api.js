572c6035452c350efdf1b9b65b009dbac1dca233
Trian Saputra <trian4869@gmail.com>
2026-07-16 16:47:47 +0700

    fix: update konfigurasi axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://6a531bae78ecba6073e2fae8.mockapi.io/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
